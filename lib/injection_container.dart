598d5800646d9cc91c9a6eabf9fae72f6dbbaf38
Navid Rahman <navidrahman92@gmail.com>
2025-04-23 00:03:55 +0600

    Update injection_container.dart

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,31 @@
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+
+import 'features/product/data/datasources/product_remote_data_source.dart';
+import 'features/product/data/repositories/product_repository_impl.dart';
+import 'features/product/domain/repositories/product_repository.dart';
+import 'features/product/domain/usecases/get_products.dart';
+import 'features/product/presentation/blocs/product_bloc.dart';
 
 final sl = GetIt.instance;
 
-Future<void> init() async {}
+Future<void> init() async {
+  // External
+  sl.registerLazySingleton(() => http.Client());
+
+  // Data sources
+  sl.registerLazySingleton<ProductRemoteDataSource>(
+    () => ProductRemoteDataSourceImpl(client: sl()),
+  );
+
+  // Repositories
+  sl.registerLazySingleton<ProductRepository>(
+    () => ProductRepositoryImpl(remoteDataSource: sl()),
+  );
+
+  // Use cases
+  sl.registerLazySingleton(() => GetProducts(sl()));
+
+  // Blocs
+  sl.registerFactory(() => ProductBloc(getProducts: sl()));
+}
